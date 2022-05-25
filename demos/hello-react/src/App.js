0bcd4d204b84c021e6663804964b1ac6c6e46331
Ernest Walzel <ewalzel@gmail.com>
2022-05-25 14:41:23 +0200

    Minor tweaks (test)

--- demos/hello-react/src/App.js
+++ demos/hello-react/src/App.js
@@ -1,34 +1,40 @@
 const styles = {
   App: {
-    textAlign: 'center'
+    textAlign: "center",
   },
   AppLogo: {
-    height: '128px',
-    marginBottom: '32px',
-    pointerEvents: 'none'
+    height: "128px",
+    marginBottom: "32px",
+    pointerEvents: "none",
   },
   AppHeader: {
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '18px',
-    color: 'white',
-    lineHeight: '32px',
-    fontWeight: '200',
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "18px",
+    fontFamily: "Open Sans",
+    color: "white",
+    lineHeight: "32px",
+    fontWeight: "200",
   },
   AppLink: {
-    color: '#61dafb',
-  }
+    color: "#0090D0",
+  },
 };
 
 function App() {
   return (
     <div style={styles.App} className="App">
       <div style={styles.AppHeader} className="App-header">
-        <img src="https://img.alicdn.com/imgextra/i4/O1CN01aT5xVM1lbORGlfbfn_!!6000000004837-2-tps-492-437.png" style={styles.AppLogo} className="App-logo" alt="logo" />
+        <img
+          src="https://img.alicdn.com/imgextra/i4/O1CN01aT5xVM1lbORGlfbfn_!!6000000004837-2-tps-492-437.png"
+          style={styles.AppLogo}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <span>src/App.js</span> and save to reload.
         </p>
