6d0254c7a1de84333125eb2fda72843bc566aec9
Ernest Walzel <ewalzel@gmail.com>
2022-06-07 10:06:51 +0200

    Access event differently

--- demos/hello-react/src/App.js
+++ demos/hello-react/src/App.js
@@ -57,7 +57,7 @@ function App() {
           <input
             type="text"
             value={value}
-            onInput={(e) => setValue(e.target.value)}
+            onInput={(e) => setValue(e.value)}
             style={{ display: "block" }}
           />
         </p>
